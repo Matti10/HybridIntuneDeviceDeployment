ca5ff7fd1549681289a26a3dda049b5b5be696e8
Matt Winsen Admin <admin.mwinsen@tricare.com.au>
2024-07-12 09:52:23 +1000

    bug fix due to stupidity

--- src/AD/Remove-DeviceADDuplicate.ps1
+++ src/AD/Remove-DeviceADDuplicate.ps1
@@ -15,12 +15,9 @@ function Remove-DeviceADDuplicate {
 		#get ad Comp
 		try {
 			$ADComp = Get-ADComputer -Identity $buildInfo.AssetID -ErrorAction SilentlyContinue
+			$ADComp.Name | Remove-ADDevice -WhatIf:$WhatIfPreference
 		} catch {
 			Write-Verbose "No device with name $($buildInfo.AssetID) exists in AD"
-		}
-		
-		if ($null -ne $ADComp) {
-			$ADComp.Name | Remove-ADDevice -WhatIf:$WhatIfPreference
 		}
 		
 		# add note to ticket that AD removal commands completed
