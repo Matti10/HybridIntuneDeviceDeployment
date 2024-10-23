7bc1f31658e6f7322385b6359a91a9d3b79f90e7
Matt Winsen <Matt.Winsen@tricare.com.au>
2024-10-24 08:08:52 +1000

    working

---
+++ tests/Security.Tests.ps1
@@ -0,0 +1,27 @@
+BeforeAll {
+
+    Set-StrictMode -Version 2.0
+    <# --- Import TriCare-Common --- #>
+    if ($PSCommandPath -like "*Script-Dev*" -or "" -eq $PSCommandPath ) {
+		Import-Module "\\tricaread\public\UsersH$\Mwinsen\Script-Dev\TriCare-Common\TriCare-Common.psm1"  -Force
+		Import-Module "\\tricaread\public\UsersH$\Mwinsen\Script-Dev\TriCare-DeviceDeployment\TriCare-DeviceDeployment.psm1"  -Force
+    } else {
+		Import-Module TriCare-Common | Out-Null
+		Import-Module TriCare-DeviceDeployment | Out-Null
+    }
+}
+BeforeDiscovery {
+	# Import-Module "\\tricaread\public\UsersH$\Mwinsen\Script-Dev\TriCare-Common\TriCare-Common.psm1"
+	# Import-Module "\\tricaread\public\UsersH$\Mwinsen\Script-Dev\TriCare-DeviceDeployment\TriCare-DeviceDeployment.psm1"
+
+
+}
+
+Describe "Getting Credentails from KV" -ForEach @(
+)  {
+	Context "" {
+		It "" {
+
+		}
+	}
+}
